8f1ea7987b8ecb5410934cd0655ca2bdc929c41f
angeldesousa <leonelprogramacion95@gmail.com>
2024-05-27 16:58:25 -0300

    codigo de prueba

--- prueba.js
+++ prueba.js
@@ -1,1 +1,7 @@
-console.log("aprendiendo un poco sobre github")
+let numeros = [5, 2, 8, 1, 9];
+
+numeros.sort(function(a, b){
+    return a - b;
+});
+
+console.log(numeros); // Imprimirá [1, 2, 5, 8, 9]
